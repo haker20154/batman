ed1b881c75e3f75f8f70882d375d33614ddf24c7
haker20154 <behrang.parsa94@gmail.com>
2016-11-25 09:02:34 +0330

    Update dads.lua

--- plugins/dads.lua
+++ plugins/dads.lua
@@ -1,7 +1,7 @@
 do
 
 function run(msg, matches)
-send_contact(get_receiver(msg), "+63 908 002 4448", "بابا", "جون", ok_cb, true)
+send_contact(get_receiver(msg), "+989386282411", "بابا", "جون", ok_cb, true)
 end
 
 return {
